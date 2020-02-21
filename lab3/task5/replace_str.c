74ce96cf8b280b09f3365cc2a495093ddd2abc4b
galefoox <foox.gal.e@gmail.com>
2020-02-20 19:46:54 -0500

    asdf

--- lab3/task5/replace_str.c
+++ lab3/task5/replace_str.c
@@ -1,29 +1,41 @@
 /* Overwrites an inputted string with 232 is awesome! if there is room.
  * * Does nothing if there is not. */
 #include <stdio.h>
+#include<stdlib.h>
+#include<string.h>
 
-void CS232(char* src) {
-     char *srcptr, *replaceptr;
-     // char *srcptr, *replaceptr;
-     int srcLength = sizeof(src) / sizeof(src[0]);
-     char *replacement[15] = {"232 is awesome!"};
+
+void CS232(char* src , int srcLength) {
+     //char *srcptr, *replaceptr;
+      char *srcptr, *replaceptr;
+	 int srcLength = strlen(src);
+     char replacement[15] = {"232 is awesome!"};
      srcptr = src;
-     replaceptr = *replacement;
-     if (srcLength >= 15) {
-         for (int i = 0; i < 15; i++)
-	      *srcptr++ = *replaceptr++;
-     }
+     replaceptr = replacement;
+	 if (srcLength >= 16)
+	 {
+	 
+		for (int i = 0; i < srcLength; i++)
+		{
+			*srcptr++ = *replaceptr++;
+		}
+
+	}
+
 }
 
 char * replace_string() {
-    char *str[15];
-    CS232(str[15]);
-    return *str;
+    char* str = (char*) malloc (16);
+    CS232(str , 16);
+    return str;
+
+	
 
 }
 
 int main(int argc, char ** argv) {
-    char ret = *replace_string();
-    printf("replaced string = %p\n", &ret);
+    char *ret = replace_string();
+    printf("replaced string = %p\n", ret);
     return 0;
+
 }
