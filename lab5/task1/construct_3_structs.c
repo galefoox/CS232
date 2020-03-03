1a27ace66d818934d058fe6e0e23aa73ec689aff
galefoox <foox.gal.e@gmail.com>
2020-03-03 18:30:50 -0500

    runs but mem leak

--- lab5/task1/construct_3_structs.c
+++ lab5/task1/construct_3_structs.c
@@ -3,21 +3,56 @@
 #include "snode.h"
 typedef struct snode node_t;
 
-node_t * setup() {
-    node_t * head ;
-    //TODO:head declared for you
+node_t* setup() {
+    node_t* head ;//head declared for you
+
+
+
     //Allocate three more pointers
+    node_t* node1;
+    node_t* node2;
+    node_t* node3;
+
     //head for the first Node, and temporary pointers node1, node2 and node3
     //Allocate three node pointees and store references to them in the three pointers
-    //Dereference each pointer to store the appropriate number and string into the length and str fields in its pointee.
+    node1 = (node_t*) malloc (sizeof(node_t));
+    node2 = (node_t*) malloc (sizeof(node_t));
+    node3 = (node_t*) malloc (sizeof(node_t));
+
+    //Dereference each pointer to store the appropriate number into the value field in its pointee.
     //Dereference each pointer to access the .next field in its pointee, 
     //and use pointer assignment to set the .next field to point to the appropriate Node. 
     
+    head = node1;
+   
+    strcpy(node1->str, "hello"); //dereferencing -> this is the same as doing (*node1).str
+    node1->length = 5;
+    node1->next = node2;
+
+    strcpy(node2->str, "there");
+    node2->length = 5;
+    node2->next = node3;
+
+    strcpy(node3->str, "prof");
+    node3->length = 4;
+    node3->next = NULL;
+
+   
+
    return head;
 }
 
-void teardown() {
-    //TODO: free all dynamic memory you requested.
+void teardown(node_t* head) {
+
+    node_t* node1 = head->next;
+    node_t* node2 = node1->next;
+    node_t* node3 = node2->next;
+
+    free(node3);
+    free(node2);
+    free(node1);
+   
+   
 }
 
 //You can ignore the following code for testing
@@ -25,7 +60,7 @@ void dump_all(node_t*);
 int main (int argc, char ** argv) {
     node_t * head = setup();
     dump_all(head);
-    teardown();
+    teardown(head);
     return 0;
 }
 
