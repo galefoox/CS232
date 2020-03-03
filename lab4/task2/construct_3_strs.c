b733b6cf173b5def086602d110e757d7fc7f0802
galefoox <foox.gal.e@gmail.com>
2020-03-02 22:14:02 -0500

    task 2 ec

--- lab4/task2/construct_3_strs.c
+++ lab4/task2/construct_3_strs.c
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
@@ -75,6 +75,7 @@ int dump_all(node_t * x) {
     printf(" %s", y->value);
     node_t * z = y->next;
     printf(" %s\n", z->value);
+    
     if(z->next != x) {
     	printf("failed");
 
@@ -99,60 +100,5 @@ int dump_all(node_t * x) {
         return 0;
     }
 
-	
-=======
-#include<stdio.h>
-#include<stdlib.h>
-typedef struct node {
-    char * value; //must use dynamic allocation 
-    struct node* next;
-} node_t;
-
-node_t * construct_3_strs() {
-
-
-    node_t *x,*y,*z;
-    x = (node_t *) malloc (sizeof(node_t));
-    y = (node_t *) malloc (sizeof(node_t));
-    z = (node_t *) malloc (sizeof(node_t));
-
-    x->value = "CS232";
-	x->next = y;
-    
-    y-> value = "is";
-	y->next = z;  
-	
-    z->value = "awesome";
-	z->next = x;
-
-	return x;
-	
 }
-
-//You can ignore the following code for testing
-int dump_all(node_t*);
-int main (int argc, char ** argv) {
-    node_t * x = construct_3_strs();
-    return dump_all(x);
-
-}
-
-int dump_all(node_t * x) {
-    printf("x -> %s", x->value);
-    node_t * y = x->next;
-    printf(" %s", y->value);
-    node_t * z = y->next;
-    printf(" %s\n", z->value);
-    if(z->next != x) {
-    	printf("failed");
-	return -1;
-    } else {
-
-		free(x);
-		free(y);
-		free(z);
-        return 0;
-    }
 	
->>>>>>> 035b7119f5b68f0a61a661de3dd26e2479bfe3d8
-}
