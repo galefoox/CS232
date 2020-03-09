276fdf38137e831a809eccf417b1aacf777b4871
CJ <xf97719n@pace.edu>
2020-03-08 20:04:18 -0400

    Task 5

---
+++ lab5/task5/snode.h
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#ifndef _snode_H_
+#define _snode_H_
+
+typedef struct snode {
+  //TODO: change str to dynamic allcoation
+  void * data;
+
+  struct snode *next;
+}snode_t;
+
+//TODO: change prototypes of functions to remove
+//length
+snode_t *snode_create(char *s) ; 
+void snode_destroy(snode_t * s) ;
+
+ 
+ #endif
